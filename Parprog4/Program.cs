5852fb2610b09a6f5343281c0403e4387cd77c64
Northorium <63539373+Northorium@users.noreply.github.com>
2024-12-06 11:50:15 +0100

    parprog
    refactoring, new year sorting, and errors management

---
+++ Parprog4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Par_progUke4;
+
+namespace Par_progUke4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new CarShop();
+        }
+    }
+}
